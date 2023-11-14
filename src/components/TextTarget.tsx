46a9299c1dc1a2954d07e127d04bce9fa6277dc1
Eddy Santiago <eddysantiagogh@gmail.com>
2023-11-13 20:03:25 -0500

    feat: implement name of lenguage to text area

--- src/components/TextTarget.tsx
+++ src/components/TextTarget.tsx
@@ -1,11 +1,17 @@
-export const TextTarget = () => (
-  <div className="h-1/2 bg-zinc-200">
+interface TextTargetProps {
+  selectedOption: string;
+}
+
+export const TextTarget: React.FC<TextTargetProps> = ({ selectedOption }) => (
+  <div className="h-1/2 relative texttarget__selected">
+    <span className="absolute text-[#27282E] bg-[#E5E7EB] border-2 border-[#ADB1B8] text-xs font-medium right-[10px] top-[9px] px-2 py-[4px] rounded-md">
+      {selectedOption}
+    </span>
     <textarea
-      className="px-2 py-2 bg-zinc-300 text-zinc-700 w-full h-full"
+      className="px-2 py-2 bg-[#F3F4F9] rounded-md text-zinc-700 w-full h-full"
       name="text-target"
       id="text-target"
       cols={30}
     ></textarea>
   </div>
-)
-
+);
